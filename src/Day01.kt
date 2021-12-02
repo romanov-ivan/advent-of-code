cd4ce633e01756aadcea26a6743dabdd3e0c7509
Ivan Romanov <romanov.ekb@gmail.com>
2021-12-02 12:52:03 +0100

    Day1 part2 implemented

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int = input.map { it.trim().toInt() }
-        .fold(0 to Int.MAX_VALUE) { (result, lastValue): Pair<Int, Int>, element: Int ->
-            (if (element > lastValue) result + 1 else result) to element
-        }.first
-
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+fun part1(input: List<String>): Int =
+    input.toIntArray().increaseCount()
+
+fun part2(input: List<String>): Int =
+    input.toIntArray().windowed(3, 1, false, List<Int>::sum).increaseCount()
+
+
+private fun List<String>.toIntArray() = map { it.trim().toInt() }
+private fun List<Int>.increaseCount() =
+    fold(0 to Int.MAX_VALUE) { (result, lastValue): Pair<Int, Int>, element: Int ->
+        (if (element > lastValue) result + 1 else result) to element
+    }.first
